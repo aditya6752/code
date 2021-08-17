70c2f90b13b60a1bfe8f61586718fc5e1463c0cb
aditya6752 <aditya.mishra6752@gmail.com>
2021-08-17 17:09:51 +0530

    completed

---
+++ oops_lad_coding/tut75.cpp
@@ -0,0 +1,14 @@
+//genral set of opeartion applied on various types of adata
+//ex-if suppose we want to find the maximum values of two values 
+//and then same operation is to be applied on float values
+//rather than amking two funxtions we cam do it on only
+#include<bits/stdc++.h>
+using namespace std;
+template<typename mytype> void display(mytype x){
+    cout<<"you have passed"<<x<<endl;
+} 
+int main()
+{
+    display(10);
+    
+}
